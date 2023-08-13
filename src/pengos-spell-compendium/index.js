3c75fd49a497d8a8c7d839cac13d32ac6743e718
pengo1998 <pengo1998@gmail.com>
2023-08-13 15:45:13 -0500

    used of hook init removed spell js file for testing

--- src/pengos-spell-compendium/index.js
+++ src/pengos-spell-compendium/index.js
@@ -1,5 +1,5 @@
-console.log("Loading pengos-spell-compendium spells...");
-
-
-
-console.log("Done loading pengos-spell-compendium spells.");
+Hooks.on("init", async function() {
+    console.log("Loading pengos-spell-compendium spells...");
+    
+    console.log("Done loading pengos-spell-compendium spells.");
+});
